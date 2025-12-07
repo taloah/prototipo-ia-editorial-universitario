93b4ebe4c1bbaea27a821d371893471add76493c
Jean Perez <jean.perez@cu.ucsg.edu.ec>
2025-12-07 15:16:42 -0500

    Limpieza de archivos por defecto de instalación innecesarios.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
+import theme from './theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
